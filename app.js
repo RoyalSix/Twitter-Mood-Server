3dd9d226510d53da9a8a54ec3599e34962054836
RoyalSix <jay.a.scottjr@biola.edu>
2017-05-11 01:29:50 -0700

    added port

--- app.js
+++ app.js
@@ -131,7 +131,7 @@ db.connect(db.MODE_PRODUCTION, function (err) {
     process.exit(1)
   } else {
     app.listen(8080, function () {
-      console.log('Listening on port 3000...')
+      console.log('Listening on port 8080...')
     })
   }
 })
