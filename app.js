0bc6892058e05d7f135fff7fe669a1e21d034fd8
RoyalSix <jay.a.scottjr@biola.edu>
2017-05-11 09:49:35 +0000

    added headers for cors

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 require('babel-register');
 var express = require('express');
 const app = express();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 var db = require('./db')
 
